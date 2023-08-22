44d9f23ac3d6f5c45d912a237ef78493dd46aa0b
snk-king <snksnk33322@gmail.com>
2023-08-22 21:56:09 +0530

    made script inside html

--- src/script.js
+++ src/script.js
@@ -1,14 +1,1 @@
 
-function setActiveTab(tab,contentId) {
-
-    const tabLinks = document.querySelectorAll('.tab-link');
-    tabLinks.forEach(link => link.classList.remove('active-tab'));
-
-    tab.classList.add('active-tab');
-
-    const content= document.querySelectorAll('.tab-contents');
-    content.forEach(link => link.classList.remove('active-content'));
-
-    const activeContent=document.getElementById(contentId);
-    activeContent.classList.add('active-content')
-}
