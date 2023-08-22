22f82e9f55f5cd5d6b6866fd7fd8061ea9ab7991
snk-king <snksnk33322@gmail.com>
2023-08-22 20:57:22 +0530

    second commit to addition of service section

--- src/script.js
+++ src/script.js
@@ -1,13 +1,13 @@
-function setActiveTab(tab) {
+function setActiveTab(tab,contentId) {
 
     const tabLinks = document.querySelectorAll('.tab-link');
     tabLinks.forEach(link => link.classList.remove('active-tab'));
 
     tab.classList.add('active-tab');
-}
-function setActiveContent(content) {
-    const tabContent =document.querySelectorAll('.tab-contents');
-    tabContent.forEach(link => link.classList.remove('active-content'));
 
-    content.classList.add('active-content');
+    const content= document.querySelectorAll('.tab-contents');
+    content.forEach(link => link.classList.remove('active-content'));
+
+    const activeContent=document.getElementById(contentId);
+    activeContent.classList.add('active-content')
 }
